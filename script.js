584909fd97f05948dadb58c71de217be36d0df85
Clara <clarapachecodev@gmail.com>
2022-09-15 09:20:56 -0300

    Code refactored and added click events for buttons set and stop

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-let play = document.querySelector('.play');
-let pause = document.querySelector('.pause');
+const buttonPlay = document.querySelector('.play');
+const buttonPause = document.querySelector('.pause');
+const buttonStop = document.querySelector('.stop');
+const buttonSet = document.querySelector('.set');
 
-play.addEventListener('click', function(){
-  play.classList.add('hide');
-  pause.classList.remove('hide');
+
+buttonPlay.addEventListener('click', function(){
+  buttonPlay.classList.add('hide');
+  buttonPause.classList.remove('hide');
+  buttonSet.classList.add('hide');
+  buttonStop.classList.remove('hide');
 
 })
 
-pause.addEventListener('click', function(){
-  pause.classList.add('hide');
-  play.classList.remove('hide');
+buttonPause.addEventListener('click', function(){
+  buttonPause.classList.add('hide');
+  buttonPlay.classList.remove('hide');
 })
 
 
+buttonStop.addEventListener('click', function(){
+  buttonPlay.classList.remove('hide');
+  buttonPause.classList.add('hide');
+  buttonSet.classList.remove('hide');
+  buttonStop.classList.add('hide');
 
+})
 
